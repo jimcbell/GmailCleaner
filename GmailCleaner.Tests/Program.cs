db0f2057715467c5315d2c3003277e4fcabf1a2f
Jim Campbell <jimcampbell355@gmail.com>
2023-12-02 17:14:25 -0500

    test updates

--- GmailCleaner.Tests/Program.cs
+++ GmailCleaner.Tests/Program.cs
@@ -6,28 +6,29 @@ using Microsoft.EntityFrameworkCore;
 Console.WriteLine("Hello, World!");
 DbContextOptionsBuilder<GmailCleanerContext> options = new();
 options.UseSqlServer("Data Source=.;Initial Catalog=GmailCleaner;Integrated Security=true;TrustServerCertificate=true;");
-//using (GmailCleanerContext context = new(options.Options))
-//{
-//    context.Database.EnsureCreated();
-//    GCUser Bob = new GCUser() { Name = "Bob", Email = "bob.gmail.com" };
-//    GCUserToken gCUserToken = new GCUserToken()
-//    {
-//        AccessToken = "asdfasdf",
-//        ExpiresOn = DateTime.Now,
-//        IdToken = "asdfasdf",
-//        RefreshToken = "asdfasdf",
-//    };
-//    Bob.GCUserTokens.Add(gCUserToken);
-//    context.Gcusers.Add(Bob);
-//    context.SaveChanges();
-
-//}
 using (GmailCleanerContext context = new(options.Options))
 {
-    int tokenId = 0;
-    GCUserToken? token = context.GcuserTokens.Where(t => t.UserTokenId == tokenId).Include(x => x.User).FirstOrDefault(x => x.UserTokenId == tokenId);
-    GCUser? user = token.User;
+    context.Database.EnsureCreated();
+    GCUser Bob = new GCUser() { GmailId = "asdfsdfas", Name = "Bob", Email = "bob.gmail.com" };
+    GCUserToken gCUserToken = new GCUserToken()
+    {
+        AccessToken = "asdfasdf",
+        ExpiresOn = DateTime.Now,
+        IdToken = "asdfasdf",
+        RefreshToken = "asdfasdf",
+    };
+    Bob.GCUserTokens.Add(gCUserToken);
+    context.GCUsers.Add(Bob);
+    context.SaveChanges();
 }
+
+    //}
+    //using (GmailCleanerContext context = new(options.Options))
+    //{
+    //    int tokenId = 0;
+    //    GCUserToken? token = context.GcuserTokens.Where(t => t.UserTokenId == tokenId).Include(x => x.User).FirstOrDefault(x => x.UserTokenId == tokenId);
+    //    GCUser? user = token.User;
+    //}
 
 
     Console.ReadLine();
