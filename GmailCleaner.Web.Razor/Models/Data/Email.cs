983bb52e7e6ae3c0459f152b6ecc1200d133018f
Jim Campbell <jimcampbell355@gmail.com>
2023-12-03 23:52:49 -0500

    Updated email model according to google will work on parsing the payload

--- GmailCleaner.Web.Razor/Models/Data/Email.cs
+++ GmailCleaner.Web.Razor/Models/Data/Email.cs
@@ -1,4 +1,5 @@
-﻿namespace GmailCleaner.Models.Data
+﻿
+namespace GmailCleaner.Models.Data
 {
     public class Email
     {
@@ -10,6 +11,30 @@
         public int SizeEstimate { get; set; }
         public string HistoryId { get; set; } = string.Empty;
         public string InternalDate { get; set; } = string.Empty;
+        public MessagePart Payload { get; set; } = new();
+
+    }
+    public class MessagePart
+    {
+        public string PartId { get; set; } = string.Empty;  
+        public string MimeType { get; set; } = string.Empty;
+        public string Filename { get; set; } = string.Empty;
+        public List<MessagePartHeader> Headers { get; set; } = new();
+        public MessagePartBody Body { get; set; } = new();
+        public List<MessagePart> Parts { get; set; } = new();   
+    }
+
+    public class MessagePartHeader
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+
+    public class MessagePartBody
+    {
+        public string AttachmentId { get; set; } = string.Empty; 
+        public int Size { get ; set; }
+        public string Data { get; set; } = string.Empty;
 
     }
 }
