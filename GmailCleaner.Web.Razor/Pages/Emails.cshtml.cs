7537906a6b76507162c90be3edf97314f7661a51
Jim Campbell <jimcampbell355@gmail.com>
2023-12-05 19:30:52 -0500

    Added validations

--- GmailCleaner.Web.Razor/Pages/Emails.cshtml.cs
+++ GmailCleaner.Web.Razor/Pages/Emails.cshtml.cs
@@ -5,15 +5,22 @@ using GmailCleaner.Models.ExternalModels;
 using GmailCleaner.Models.Settings;
 using GmailCleaner.Services;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages; // Page
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Nodes; // JsonNode
 
 namespace GmailCleaner.Pages;
 
+[ResponseCache(Duration = 200 /* seconds */, Location = ResponseCacheLocation.Any)]
 public class EmailsModel : PageModel
 {
     private IEmailAdapter _emailAdapter;
     private IUserContextService _contextService;
+
+    [BindProperty]
+    [Range(0, 25)]
+    public int NumberEmails { get; set; } = 5;
 
     public List<Email> Emails { get; set; } = new List<Email>();
     public ErrorModel EmailErrors { get; set; } = new();
@@ -29,7 +36,7 @@ public class EmailsModel : PageModel
     }
     public PageResult OnGet(int userId)
     {
-
+        
         if (userId < 1)
         {
             RedirectToPage("Error", new { error = "User Id is required" });
@@ -45,10 +52,16 @@ public class EmailsModel : PageModel
     }
     public async Task<PageResult> OnPost(string filter = "")
     {
-
+        if (ModelState.IsValid)
+        {
+            Console.Write("Model is valid");
+        }
+        else {
+            NumberEmails = 25;
+        }
         //string accessToken = _contextService.GetToken(Request);
         //AccessToken = accessToken;
-        Emails = await _emailAdapter.GetEmails(Request, filter);
+        Emails = await _emailAdapter.GetEmails(Request, NumberEmails);
         //Response.Cookies.Append(key: "access_token", accessToken);
         return Page();
     }
