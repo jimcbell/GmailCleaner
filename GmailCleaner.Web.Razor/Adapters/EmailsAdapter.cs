09beeef2b813a032e259e37d0f48ca6000573e4b
Jim Campbell <jimcampbell355@gmail.com>
2023-12-10 18:04:22 -0500

    Updated adapter

--- GmailCleaner.Web.Razor/Adapters/EmailsAdapter.cs
+++ GmailCleaner.Web.Razor/Adapters/EmailsAdapter.cs
@@ -10,7 +10,16 @@ namespace GmailCleaner.Adapters
 {
     public interface IEmailAdapter
     {
-        public Task<List<Email>> GetEmails(HttpRequest request, int numberEmails, string filter = "");
+        /// <summary>
+        /// Gets emails from the email manager.
+        /// If there is a null response that means there was a transient error and it will return an empty list.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="numberEmails"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception">Throws an exception on a permanent api error (automatically) - should redirect to error page.</exception>"
+        public Task<List<Email>?> GetEmails(GCUser user, int numberEmails, string filter = "");
     }
     public class EmailsAdapter : IEmailAdapter
     {
@@ -28,18 +37,21 @@ namespace GmailCleaner.Adapters
             _userManager = userManager;
 
         }
-        public async Task<List<Email>> GetEmails(HttpRequest request, int numberEmails, string filter = "")
+        public async Task<List<Email>?> GetEmails(GCUser user, int numberEmails, string filter = "")
         {
-            // Functionality replaces
-            GCUser user = await getUser(request);
-
+            
             List<Email> emails = new List<Email>();
 
             string accessToken = await _accessManager.GetAccessTokenAsync(user.UserId);
             _emailRepository.LoadAccessToken(accessToken);
-            EmailMetadatas emailMetadatas = await _emailRepository.GetEmailMetadatas(filter, maxEmails: numberEmails);
-            List<string> emailIds = emailMetadatas.Metadatas.Select(e => e.Id).ToList();
-            emails = await _emailRepository.GetEmails(emailIds);
+            // This will throw an exception if there is a permanent error on google api, letting that be auto rethrown.
+            EmailMetadatas? emailMetadatas = await _emailRepository.GetEmailMetadatas(filter, maxEmails: numberEmails);
+            // If there is a transient error, it will return null, so we need to check for that.
+            if(emailMetadatas != null)
+            {
+                List<string> emailIds = emailMetadatas.Metadatas.Select(e => e.Id).ToList();
+                emails = await _emailRepository.GetEmails(emailIds, user.GmailId) ?? new();
+            }
             return emails;
         }
 
