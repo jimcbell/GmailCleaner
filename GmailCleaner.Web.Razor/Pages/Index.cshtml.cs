a570547a4d7eb1a50ee5a74b9bb879c468fb40e1
Jim Campbell <jimcampbell355@gmail.com>
2023-12-11 19:05:14 -0500

    Razor page updates

--- GmailCleaner.Web.Razor/Pages/Index.cshtml.cs
+++ GmailCleaner.Web.Razor/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Azure.Core;
 using GmailCleaner.Adapters;
 using GmailCleaner.Common.Models;
 using GmailCleaner.Managers;
@@ -15,51 +16,87 @@ namespace GmailCleaner.Pages
         private readonly ILogger<IndexModel> _logger;
         private IIdentityAdapter _homeAdapter;
         private IConfiguration _config;
-        private IEmailAdapter _emailAdapter;
+        private IMessageManager _messageManager;
 
         public bool IsAuthenticated { get; set; } = false;
-        public int UserId { get; set; } = 0;
-        public GCUser? GCUser { get; set; } = new GCUser();
+        public string? Name { get; set; }
+        public int Usages { get; set; } = 0;
+        public string ErrorMessage { get; set; } = string.Empty;
+        private string _gmailId = string.Empty;
 
-        public IndexModel(ILogger<IndexModel> logger, IIdentityAdapter homeAdapter, IConfiguration config, IEmailAdapter emailAdapter)
+        public IndexModel(ILogger<IndexModel> logger, 
+            IIdentityAdapter homeAdapter, 
+            IConfiguration config, 
+            IMessageManager messageManager)
         {
             _logger = logger;
             _homeAdapter = homeAdapter;
             _config = config;
-            _emailAdapter = emailAdapter;
+            _messageManager = messageManager;
         }
 
         public async Task<IActionResult> OnGet()
         {
-            ClaimsPrincipal claimsPrincipal = base.User;
-            if (claimsPrincipal.Identity != null && claimsPrincipal.Identity.IsAuthenticated)
+            if (isUserAuthenticated())
             {
-                var user = await _homeAdapter.GetUser(Request);
+                GCUser? user = await _homeAdapter.GetUser(Request);
+
                 if (user != null)
                 {
                     IsAuthenticated = true;
-                    GCUser = user;
-                    UserId = user.UserId;
-                    return Page();
+                    Name = user.Name;
+                    Usages = user.Usages;
+                    if(user.GCMessages.Count >0)
+                    {
+                        return RedirectToPage("Messages");
+                    }
                 }
                 else
                 {
+                    _logger.LogError($"User with gmailId was authenticated but not saved to the database: {_gmailId}");
                     return RedirectToPage("Error", new { error = "User not found" });
                 }
             }
-            else
-            {
-                return Page();
-            }
-            //if (!string.IsNullOrEmpty(accessToken))
-            //{
-            //    return RedirectToPage("Emails", accessToken, userId);
-            //}
-            //return Page();
+            return Page();
         }
         public async Task<IActionResult> OnPost()
         {
+            GCUser? user = await _homeAdapter.GetUser(Request);
+            if (!isUserAuthenticated() || user ==null)
+            {
+                return RedirectToPage("Error", new { error = "User not found" });
+            }
+            if (!(user.GCMessages.Count > 0)) // Only load messages if they haven't been loaded yet
+            {
+                try
+                {
 
+                    List<GCMessage>? messages = await _messageManager.LoadMessagesAsync(user!);
+                    if (messages == null) // Transient error getting messages
+                    {
+                        ErrorMessage = "Too many requests to Gmail. Please try again in a few minutes.";
+                        return Page();
+                    }
+                }
+                catch
+                {
+                    return RedirectToPage("Error", new { error = "Error loading messages. Please contact jimcampbell355@gmail.com to remediate account issues." });
+                }
+            }
+            return RedirectToPage("Messages");
+        }
+        private bool isUserAuthenticated()
+        {
+            ClaimsPrincipal claimsPrincipal = base.User;
+            if (claimsPrincipal.Identity != null && claimsPrincipal.Identity.IsAuthenticated)
+            {
+                _gmailId = Request.HttpContext.User.Claims.Where(c => c.Type == "id").Select(c => c.Value).FirstOrDefault() ?? string.Empty;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
---
+++ GmailCleaner.Web.Razor/Pages/Messages.cshtml.cs
@@ -0,0 +1,105 @@
+using GmailCleaner.Adapters;
+using GmailCleaner.Common.Models;
+using GmailCleaner.Managers;
+using GmailCleaner.Models;
+using GmailCleaner.Models.Data;
+using GmailCleaner.Models.ExternalModels;
+using GmailCleaner.Models.Settings;
+using GmailCleaner.Models.ViewModels;
+using GmailCleaner.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages; // Page
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Nodes;
+using System.Web; // JsonNode
+
+namespace GmailCleaner.Pages;
+
+public class MessagesModel : PageModel
+{
+    private IMessageAdapter _messageAdapter;
+    private IUserContextService _contextService;
+    private IIdentityAdapter _identityAdapter;
+
+    public List<GCMessage> Messages { get; set; } = new List<GCMessage>();
+    public ErrorModel EmailErrors { get; set; } = new();
+    private GCUser? user { get; set; } = null;
+    public List<Tuple<string, bool>> DeleteList { get; set; } = new();
+    public Dictionary<string, int> FromCounts { get; set; } = new();
+    public List<MessageViewModel> MessageViewModels { get; set; } = new();
+    public int DeleteCount { get; set; } = 0;   
+    public string ErrorMessage { get; set; } = string.Empty;    
+
+
+
+    public MessagesModel(IMessageAdapter messageAdapter, IUserContextService contextService, IIdentityAdapter identityAdapter)
+    {
+        _messageAdapter = messageAdapter;
+        _contextService = contextService;
+        _identityAdapter = identityAdapter;
+
+    }
+    public async Task<IActionResult> OnGet()
+    {
+        user = await _identityAdapter.GetUser(Request);
+        if (user == null)
+        {
+            return RedirectToPage("Error", new { error = "User Id is required" });
+        }
+        else
+        {
+            await initializePage(user);
+            
+            return Page();
+        }
+        //accessToken = _contextService.GetToken(Request);
+        //Emails = await _emailAdapter.GetEmails(accessToken, filter);
+        //Response.Cookies.Append(key: "access_token", accessToken);
+    }
+
+    public async Task<IActionResult> OnPost([FromForm] Dictionary<string, int> deleteList)
+    {
+        user = await _identityAdapter.GetUser(Request);
+        if (user != null)
+        {
+            await initializePage(user);
+            List<GCMessage> messagesToDelete =  _messageAdapter.GetMessagesToDeleteFromList(deleteList, Messages);
+            DeleteCount = await _messageAdapter.TrashMessagesAsync(messagesToDelete, user);
+            // Reinitialize the page so the deleted messages are gone.
+            await initializePage(user);
+        }
+        return Page();
+    }
+    public async Task<IActionResult> OnPostDeleteAll()
+    {
+        user = await _identityAdapter.GetUser(Request);
+        if (user != null)
+        {
+            await initializePage(user);
+            List<GCMessage> messagesToDelete = Messages;
+            DeleteCount = await _messageAdapter.DeleteMessagesAsync(user);
+            // Reinitialize the page so the deleted messages are gone.
+            await initializePage(user);
+            if(user.GCMessages.Count == 0)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ErrorMessage = "Error deleting messages from GmailCleaner. Please try again, if the issue persists, please reach out to jimcampbell355@gmail.com";
+            }
+            return Page();
+        }
+        else
+        {
+            return RedirectToPage("Error", new { error = "User Id is required" });
+        }
+    }
+    private async Task initializePage(GCUser user)
+    {
+        Messages = await _messageAdapter.GetMessagesAsync(user);
+        MessageViewModels = _messageAdapter.GetMessageViewModels(Messages);        
+    }
+    
+}
